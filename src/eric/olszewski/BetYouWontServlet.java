e91a9e58aed6aa435427ff8008d4aa7d99be38d5
ericolszewski <eolszewski@gmail.com>
2013-11-09 11:56:46 -0600

    Initial commit

---
+++ src/eric/olszewski/BetYouWontServlet.java
@@ -0,0 +1,13 @@
+package eric.olszewski;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class BetYouWontServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
